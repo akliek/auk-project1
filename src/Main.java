8bb7a0dc79efe6ee1c14390931b0b9e65d058b50
akliek <www.klekanton@gmail.com>
2023-08-27 11:21:45 +0200

    add image

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("My name is Anton. Welcome to my first Java program");
+        System.out.println("My name is Anton. Welcome to my first Java program.");
+        System.out.println("I'm happy to start solving new challenges.");
     }
 }
